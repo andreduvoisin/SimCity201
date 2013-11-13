a7e7379648f3aa1723eaf803aa26c3f3ef09abfb
Maggi Yang <maggiyan@usc.edu>
2013-11-12 19:38:46 -0800

    added restaurant_maggiyan

---
+++ src/restaurant_maggiyan/interfaces/Market.java
@@ -0,0 +1,21 @@
+package restaurant_maggiyan.interfaces;
+
+import java.util.Map;
+
+import restaurant_maggiyan.CookAgent;
+
+/**
+ * A sample Customer interface built to unit test a CashierAgent.
+ *
+ * @author Monroe Ekilah
+ *
+ */
+public interface Market {
+	
+	public void msgRequestItems(Cook c, Map<String, Integer> itemRequest);
+
+	public String getName();
+
+	public void msgHereIsPayment(Double total);
+
+}
