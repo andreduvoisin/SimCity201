337bf3c630ef31531a01e7982d1459403886687f
Shane Mileham <smileham@usc.edu>
2013-11-18 18:43:41 -0800

    Setting up party class infrastructure

---
+++ src/base/Event.java
@@ -0,0 +1,8 @@
+package base;
+
+public class Event {
+	enum EnumEventType {JOB, EAT, CAR, DEPOSIT_CHECK, PARTY};
+	EnumEventType mEvent;
+	int mTime;
+	Location mLocation;
+}
---
+++ src/base/Location.java
@@ -0,0 +1,6 @@
+package base;
+
+public class Location {
+	public int mX;
+	public int mY;
+}
