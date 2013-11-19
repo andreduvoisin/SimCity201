378b39478bdbb8be7402829737589cee62e00f54
Shane Mileham <smileham@usc.edu>
2013-11-18 22:29:52 -0800

    Added item constructor

--- src/base/Item.java
+++ src/base/Item.java
@@ -6,4 +6,10 @@ public class Item {
 	public static enum EnumMarketItemType {CHICKEN, PIZZA, SALAD, STEAK};
 	public static Map<EnumMarketItemType, Integer> sPrices;
 	//TODO: 2 make static map for prices
+	
+	EnumMarketItemType mItemType;
+	
+	public Item(EnumMarketItemType itemType){
+		mItemType = itemType;
+	}
 }
