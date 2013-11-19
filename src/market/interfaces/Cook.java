742092e01eb3effcea3be314f1000c3a3fb1bb22
Angelica Huyen Tran <tranac@usc.edu>
2013-11-18 17:12:08 -0800

    Working on market workers.

--- src/market/interfaces/Cook.java
+++ src/market/interfaces/Cook.java
@@ -1,27 +1,11 @@
 package market.interfaces;
 
 import java.util.Map;
-
-import market.other.Invoice;
-import market.other.Item.EnumMarketItemType;
-import restaurant_smileham.Order;
-import restaurant_smileham.Food.EnumFoodOptions;
-import restaurant_smileham.gui.CookGui;
-import restaurant_smileham.test.mock.EventLog;
+import market.other.*;
 
 public interface Cook {
 	
-	public EventLog log = new EventLog();
+	public abstract void msgInvoiceToPerson(Map<Item,Integer> cannotFulfill, Invoice invoice);
 	
-	//Messages
-	public abstract void msgInvoiceToPerson(Map<EnumMarketItemType, Integer> canFulfill, Invoice invoice);
-
-	//Scheduler
-	public abstract boolean pickAndExecuteAnAction();
-
-	
-	//Actions
-	
-	//Accessors
-
+	public abstract void msgHereIsCookOrder(Order o);
 }
