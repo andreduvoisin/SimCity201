b8cb478a8de0c14b68c21733b92b447c1d726a20
sciencepro <dcarr@scienceprousa.com>
2013-11-18 17:31:42 -0800

    make housing data public

--- src/housing/House.java
+++ src/housing/House.java
@@ -3,9 +3,9 @@ package housing;
 import housing.interfaces.Renter;
 
 public class House {
-	int xLocation, yLocation;
-	double mRent;
-	Renter mOccupant;
+	public int xLocation, yLocation;
+	public double mRent;
+	public Renter mOccupant;
 
 	public House(int x, int y, double rent) {
 		xLocation = x;
