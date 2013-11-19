c8c04d738ecf650d7c5ad6767f8fb12513eae3b9
sciencepro <dcarr@scienceprousa.com>
2013-11-18 22:27:43 -0800

    start HousingGui

---
+++ src/housing/gui/HouseGui.java
@@ -0,0 +1,14 @@
+package housing.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class HouseGui extends JPanel implements ActionListener {
+
+	public void actionPerformed(ActionEvent e) {
+		repaint();
+	}
+
+}
