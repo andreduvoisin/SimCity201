1533d4ee1beb36406c6c7b54e0173187c6e18432
Chase Wagoner <cwagoner@usc.edu>
2013-11-19 23:21:13 -0800

    Modified mockCashier.

--- src/market/test/mock/MockCashier.java
+++ src/market/test/mock/MockCashier.java
@@ -1,5 +1,33 @@
 package market.test.mock;
 
-public class MockCashier {
+import market.interfaces.Cashier;
+import test.mock.*;
+import market.*;
+
+public class MockCashier extends Mock implements Cashier {
+
+	@Override
+	public void msgOrderPlacement(Order order) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgPayingForOrder(Invoice invoice) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getNumWorkers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
