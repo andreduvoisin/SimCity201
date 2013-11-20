b51c02f0a416b3734ea3c5d7b4f150206792ccfe
Rex Xu <ttrojan@usc.edu>
2013-11-19 23:31:27 -0800

    Finished first GuardRole test

--- src/bank/test/GuardTest.java
+++ src/bank/test/GuardTest.java
@@ -1,18 +1,24 @@
 package bank.test;
 
 import junit.framework.TestCase;
+import bank.interfaces.Customer;
+import bank.interfaces.Teller;
+import bank.roles.BankGuardRole;
 import bank.test.mock.MockCustomerRole;
 import bank.test.mock.MockTellerRole;
 import base.PersonAgent;
+import base.interfaces.Person;
+import base.interfaces.Role;
+
 
 public class GuardTest extends TestCase{
 	
-	PersonAgent mPerson;
 //	INTERFACES
-	MockCustomerRole mCustomer1;
-	MockCustomerRole mCustomer2;
-	MockTellerRole mTeller1;
-	
+	BankGuardRole mGuard;
+	Person mPerson;
+	Customer mCustomer1;
+	Customer mCustomer2;
+	Teller mTeller1;
 	
 	/**
 	 * This method is run before each test. You can use it to instantiate the class variables
@@ -20,7 +26,16 @@ public class GuardTest extends TestCase{
 	 */
 	public void setUp() throws Exception{
 		super.setUp();
-			
+		//Testee
+		mGuard = new BankGuardRole();
+		mPerson = new PersonAgent("Person");
+		mPerson.addRole((Role)mGuard);
+		
+		//Interfaces
+		mCustomer1 = new MockCustomerRole();
+		mCustomer2 = new MockCustomerRole();
+		mTeller1 = new MockTellerRole();
+		
 		//clear logs
 	}
 	
@@ -29,8 +44,25 @@ public class GuardTest extends TestCase{
 	public void testOne_OneCustomer(){
 		//setUp()
 		
-
-
+		//Preconditions
+		assertTrue("Guard has no customers.", mGuard.mCustomers.size() == 0);
+		assertTrue("Guard has no tellers.", mGuard.mTellers.isEmpty());
+		
+		//1 : add customer & teller
+		mGuard.msgReadyToWork(mTeller1);
+		mGuard.msgNeedService(mCustomer1);
+		
+		//Check
+		assertTrue("Guard has one customer.", mGuard.mCustomers.size() == 1);
+		assertTrue("Guard has one teller.", !mGuard.mTellers.isEmpty());
+		assertTrue("Teller is available.", mGuard.mTellers.get(mTeller1));
+		
+		//2 : p.a.e.a. (provideService(c,t))
+		assertTrue("PAEA: provideService(c,t)", mGuard.pickAndExecuteAnAction());
+		
+		//Check
+		assertTrue("Guard has no customers.", mGuard.mCustomers.size() == 0);
+		assertTrue("Teller is unavailable.", !mGuard.mTellers.get(mTeller1));
 	}
 }
 
