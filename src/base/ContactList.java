4935794aa672674cfc22d0255a84def552de4839
Chase Wagoner <cwagoner@usc.edu>
2013-11-22 01:55:28 -0800

    Question\!

--- src/base/ContactList.java
+++ src/base/ContactList.java
@@ -15,4 +15,7 @@ public class ContactList {
 	static Map<Person, Location> sRestaurantHosts; //ALL: Make a host interface and implement a restaurant SHANE: 1 TELL PEOPLE TO ADD A L L
 	
 	//REX ALL: What else do we need here?
+	// SHANE: consider above maps with location as a key, instead of value
+	// so that we can look up the role-person by location
+	// (finding the location of a role isn't as helpful, methinks)
 }
