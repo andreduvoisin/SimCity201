0a92657880813daa01acaed80bd0ed188f8af157
jerryweb <jerryweb@usc.edu>
2013-11-22 17:08:41 -0800

    added futher functionality to the GUI. citycards are still presenting problems when attempting to add internal guis.

--- src/city/gui/CityCard.java
+++ src/city/gui/CityCard.java
@@ -2,17 +2,19 @@ package city.gui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 
 public class CityCard extends SimCityPanel {
 
 	public static final int CARD_WIDTH = 500, CARD_HEIGHT = 500;
-
+	//private 
 	public CityCard(SimCityGui city) {
 		super(city);
 		this.setPreferredSize(new Dimension(CARD_WIDTH, CARD_HEIGHT));
 		this.setVisible(true);
 		addMouseListener(this);
+		
 		background = Color.green;
 	}
 	
@@ -22,7 +24,21 @@ public class CityCard extends SimCityPanel {
 		this.setVisible(true);
 		addMouseListener(this);
 		background = c;
+
 	}
+	/*
+	public void paint(Graphics l) {
+		l.setColor(Color.ORANGE);
+		l.fillRect(100, 100, 20, 20);
+	}
+	/*
+	public CityCard(SimCityGui city, Color c) {
+		super(city);
+		this.setPreferredSize(new Dimension(CARD_WIDTH, CARD_HEIGHT));
+		this.setVisible(true);
+		addMouseListener(this);
+		background = c;
+	}*/
 
 
 	public void mouseClicked(MouseEvent e) {
