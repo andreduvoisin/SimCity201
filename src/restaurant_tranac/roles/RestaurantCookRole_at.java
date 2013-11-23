145c0af0be2dd13646e25d9a5bcd81f3cf206086
Angelica <tranac@usc.edu>
2013-11-22 23:54:38 -0800

    Finished base of restaurant integration. Should be able to use RestaurantPanel_at and roles with SimCity.

---
+++ src/restaurant_tranac/roles/RestaurantCookRole_at.java
@@ -0,0 +1,394 @@
+package restaurant_tranac.roles;
+
+import java.util.*;
+import java.util.concurrent.Semaphore;
+
+import base.BaseRole;
+import restaurant_tranac.gui.CookGui_at;
+import restaurant_tranac.interfaces.*;
+
+/**
+ * Restaurant Cook Agent
+ */
+public class RestaurantCookRole_at extends BaseRole implements Cook {
+	private CookGui_at cookGui;
+	
+	public enum OrderState {Pending, Cooking, Plated, PickedUp, Done, Finished};
+	public enum FoodState {Good, LowStock, Ordered, NoStock};
+	
+	private List<Market> markets = Collections.synchronizedList(new ArrayList<Market>());
+	private List<Order> orders = Collections.synchronizedList(new ArrayList<Order>());
+	private List<Food> inventory = Collections.synchronizedList(new ArrayList<Food>());
+	
+	private final int NGRILLS = 3;
+	private final int NPLATES = 4;
+	private Map<Integer, Boolean> grills = new HashMap<Integer, Boolean>();
+	private Map<Integer, Boolean> plates = new HashMap<Integer, Boolean>();
+	
+	private Timer timer = new Timer();
+	
+	private final int baseTime = 5000;
+	private final int baseInventory = 2;
+	private final int baseNeed = 3;
+	private final int stockThreshold = 1;
+	
+	private Semaphore inTransit = new Semaphore(0, true);
+	
+	public RestaurantCookRole_at() {
+		super();
+		
+		//create inventory
+		inventory.add(new Food("Steak",(int)(baseTime*2),baseInventory));
+		inventory.add(new Food("Chicken",(int)(baseTime*1.75),baseInventory));
+		inventory.add(new Food("Salad",baseTime,baseInventory));
+		inventory.add(new Food("Pizza",(int)(baseTime*1.5),baseInventory));
+		
+		//initialize grills and plates
+		for(int i=1;i<=NGRILLS;i++)
+			grills.put(i,false);
+		for(int i=1;i<=NPLATES;i++)
+			plates.put(i, false);
+	}
+
+	/** Messages */
+
+	public void msgHereIsOrder(Waiter w, String c, int t) {
+		orders.add(new Order(w,c,t));
+		stateChanged();
+	}
+	
+	public void msgOrderPickedUp(Waiter w, String c) {
+		synchronized(orders) {
+			for(Order o : orders) {
+				if(o.waiter == w && o.choice.equals(c)) {
+					o.s = OrderState.PickedUp;
+				}
+			}
+		}
+		stateChanged();
+	}
+	
+	public void msgOrderDone(Order o) {
+		o.s = OrderState.Done;
+		stateChanged();
+	}
+	
+	public void msgCanFulfillInventory(String f, int n) {
+		Food food = null;
+		synchronized(inventory) {
+			for(Food i : inventory) {
+				if(i.name == f)
+					food = i;
+			}
+		}
+		food.s = FoodState.Ordered;
+		if(n < food.numNeeded) {
+			food.s = FoodState.LowStock;
+			food.numNeeded = food.numNeeded - n;
+		}
+		else {
+			food.numNeeded = 0;
+		}
+		stateChanged();
+	}
+	
+	public void msgHereIsInventory(String f, int n) {
+		Food food = null;
+		synchronized(inventory) {
+			for(Food i : inventory) {
+				if(i.name == f)
+					food = i;
+			}
+		}
+		food.s = FoodState.Good;
+		food.stock = food.stock + n;
+		stateChanged();
+	}
+	
+	public void msgOutOfInventory(Market m, String f) {
+		Food food = null;
+		synchronized(inventory) {
+			for(Food i : inventory) {
+				if(i.name == f)
+					food = i;
+			}
+		}
+		food.s = FoodState.LowStock;
+		food.addMarketOutOfItem(m);
+		stateChanged();
+	}
+	
+	/** Animation Messages */
+	public void msgAnimationAtGrill() {
+		inTransit.release();
+		stateChanged();
+	}
+	
+	public void msgAnimationAtPlate() {
+		inTransit.release();
+		stateChanged();
+	}
+	
+	/**
+	 * Scheduler.  Determine what action is called for, and do it.
+	 */
+	public boolean pickAndExecuteAnAction() {
+		for(Food f : inventory) {
+			if(f.s == FoodState.LowStock) {
+				orderFood();
+				return true;
+			}
+		}
+		for(Order o : orders) {
+			if(o.s == OrderState.Pending) {
+				tryToCookIt(o);
+				return true;
+			}
+		}
+		for(Order o : orders) {
+			if(o.s == OrderState.PickedUp) {
+				removeOrder(o);
+				return true;
+			}
+		}
+		for(Order o : orders) {
+			if(o.s == OrderState.Done) {
+				plateIt(o);
+				return true;
+			}
+		}
+		DoGoToHome();
+		return false;
+	}
+
+	/** Actions */
+
+	private void tryToCookIt(final Order o) {
+		Food food = null;
+		synchronized(inventory) {
+			for(Food i : inventory) {
+				if(i.name == o.choice)
+					food = i;
+			}
+		}
+		if(food.stock == stockThreshold && food.s != FoodState.Ordered) {
+			food.s = FoodState.LowStock;
+			Do("Low on food!");
+			food.numNeeded = baseNeed;
+		}
+		
+		if(food.stock == 0) {				//handles if out of food item
+			Do("Out of " + food.name);
+			o.waiter.msgOutOfFood(o.choice, o.table);
+			orders.remove(o);
+			if(food.s != FoodState.NoStock && food.s != FoodState.Ordered) {
+				food.s = FoodState.LowStock;
+				food.numNeeded = baseNeed;
+			}
+			return;
+		}
+		
+		Do("Cooking " + o.choice);
+		o.s = OrderState.Cooking;
+
+		for(int i=1;i<=NGRILLS;i++) {
+			if(!grills.get(i)) {
+				o.n = i;
+				grills.put(i, true);
+				break;
+			}
+		}
+		if(o.n == 0)
+			o.n = 1;
+		
+		DoGoToGrill(o);
+		DoAddFoodItem(o);
+		timer.schedule(new TimerTask() {	//runs a new timer to "cook" the food
+			public void run() {
+				msgOrderDone(o);
+			}
+		},food.cookingTime);
+		food.stock--;
+	}
+	
+	private void plateIt(Order o) {
+		Do("Plating " + o.choice);
+		o.s = OrderState.Plated;
+		DoGoToGrill(o);
+		grills.put(o.n, false);
+		for(int i=1;i<=NPLATES;i++) {
+			o.n = 0;
+			if(!plates.get(i)) {
+				o.n = i;
+				plates.put(i, true);
+				break;
+			}
+		}
+		if(o.n == 0)
+			o.n = 1;
+		
+		DoGoToPlate(o);
+		o.waiter.msgOrderDone(o.choice, o.table, o.n);	//messages waiter order is done
+	}
+
+	private void removeOrder(Order o) {
+		Do("Removing " + o.choice);
+		o.s = OrderState.Finished;
+		DoRemoveOrder(o);
+		plates.put(o.n, false);
+		o.n = 0;
+	}
+	
+	private void orderFood() {				//order all food items that are lowStock
+		Do("Ordering food");
+		synchronized(inventory) {
+			for(Food f : inventory) {
+				if(f.s == FoodState.LowStock) {
+				synchronized(markets) {
+					for(Market m : markets) {
+						if(!f.outOfItem.isEmpty()) {
+							for(Market out : f.outOfItem) {
+								if(m != out) {
+									f.s = FoodState.Ordered;
+									m.msgOrderFood(this,f.name,f.numNeeded);
+									break;
+								}
+							}
+						if(f.s == FoodState.Ordered)
+							break;
+						}
+						else {
+							f.s = FoodState.Ordered;
+							m.msgOrderFood(this,f.name,f.numNeeded);
+							break;
+						}
+					}
+				}
+				if(f.s != FoodState.Ordered)
+					f.s = FoodState.NoStock;			//if all markets are out of stock, cannot order more
+				}
+			}
+		}
+	}
+	
+	/** Animation Actions */
+	private void DoGoToHome() {
+		cookGui.DoGoToHome();
+	}
+	
+	private void DoAddFoodItem(Order o) {
+		cookGui.DoAddFoodItem(o.choice, o.n);
+	}
+	
+	private void DoGoToGrill(Order o) {
+		cookGui.DoGoToGrill(o.n);
+		try {
+			inTransit.acquire();
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void DoGoToPlate(Order o) {
+		cookGui.DoGoToPlate(o.n, o.choice);			//change to choose correct plate
+		try {
+			inTransit.acquire();
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void DoRemoveOrder(Order o) {
+		cookGui.FoodPickedUp(o.n, o.choice);
+	}
+	
+	/** Utilities */
+
+	public String getName() {
+		return mPerson.getName();
+	}
+	
+	public void setGui(CookGui_at c) {
+		setCookGui(c);
+	}
+	
+	public CookGui_at getCookGui() {
+		return cookGui;
+	}
+
+	public void setCookGui(CookGui_at cookGui) {
+		this.cookGui = cookGui;
+	}
+	
+	public void addMarket(Market m) {
+		markets.add(m);
+	}
+	
+	public void setInventory(int n) {
+		synchronized(inventory) {
+			for(Food f : inventory) {
+				f.stock = n;
+				if(n <= stockThreshold) {
+					f.s = FoodState.LowStock;
+					f.numNeeded = baseNeed;
+				}
+			}
+		}
+	}
+	
+	public void setInventory(String f, int n) {
+		synchronized(inventory) {
+			for(Food food : inventory) {
+				if(food.name.equals(f)) {
+					food.stock = n;
+					if(n <= stockThreshold) {
+						food.s = FoodState.LowStock;
+						food.numNeeded = baseNeed;
+					}
+				}
+			}
+		}
+	}
+
+	/** Classes */
+	
+	private class Order {		//holds all relevant information for the order
+		Waiter waiter;
+		String choice;
+		int table;
+		int n;
+		OrderState s;
+		
+		Order(Waiter w, String c, int t) {
+			this.waiter = w;
+			choice = c;
+			table = t;
+			n = 0;
+			s = OrderState.Pending;
+		}
+	}
+	
+	private class Food {
+		String name;
+		int cookingTime;
+		int stock;
+		int numNeeded;
+		FoodState s;
+		List<Market> outOfItem = new ArrayList<Market> ();
+		
+		Food(String n, int c, int s) {
+			name = n;
+			cookingTime = c;
+			stock = s;
+			this.s = FoodState.Good;
+			numNeeded = 0;
+		}
+		
+		public void addMarketOutOfItem(Market m) {
+			outOfItem.add(m);
+		}
+	}
+}
+
---
+++ src/restaurant_tranac/roles/RestaurantHostRole_at.java
@@ -0,0 +1,420 @@
+package restaurant_tranac.roles;
+
+import base.BaseRole;
+import restaurant_tranac.gui.HostGui_at;
+import restaurant_tranac.interfaces.*;
+
+import java.util.*;
+
+
+/**
+ * Restaurant Host Agent
+ */
+
+public class RestaurantHostRole_at extends BaseRole implements Host{
+	private HostGui_at hostGui;
+	
+	static final int NTABLES = 4;		//number of tables in rest
+	static final int NWAITINGAREA = 20;
+	public Collection<Table> tables;
+	private int numWaiters;
+
+	//list of agents interacting in the restaurant
+	public List<MyCustomer> customers = Collections.synchronizedList(new ArrayList<MyCustomer>());
+	public List<MyWaiter> waiters = Collections.synchronizedList(new ArrayList<MyWaiter>());
+
+	private Map<Integer, MyCustomer> waitingAreas = new HashMap<Integer, MyCustomer>(NWAITINGAREA);
+	//MyCustomerState
+	enum CustomerState {Arrived, WillWait, Walking, Waiting, Debating, Leaving, Seating, Seated, Eating, Finished, Done};
+	//MyWaiterState
+	enum WaiterState {Active, WantToGoOnBreak, OnBreak};
+	
+	public RestaurantHostRole_at() {
+		super();
+
+		//create the list of tables
+		tables = new ArrayList<Table>(NTABLES);
+		for(int i=1;i<= NTABLES;i++) {
+			tables.add(new Table(i));
+		}
+		
+		for(int i=0;i<NWAITINGAREA;i++) {
+			waitingAreas.put(i,null);
+		}
+		numWaiters = 0;
+	}
+
+	/** Messages */
+	public void msgIWantFood(Customer c) {
+		//check if the customer is a returning customer
+		synchronized(customers) {
+			for(MyCustomer customer : customers) {
+				if(customer.c == c) {
+					customer.s = CustomerState.Arrived;
+					stateChanged();
+					return;
+				}
+			}
+		}
+		//else, create a new MyCustomer
+		synchronized(customers) {
+			customers.add(new MyCustomer(c,CustomerState.Arrived));
+		}
+		stateChanged();
+	}
+	
+	public void msgWillWait(Customer c) {
+		synchronized(customers) {
+			for(MyCustomer customer: customers) {
+				if(customer.c == c) {
+					customer.s = CustomerState.WillWait;
+					stateChanged();
+				}
+			}
+		}
+	}
+	
+	public void msgLeavingEarly(Customer c) {
+		synchronized(customers) {
+			for(MyCustomer customer: customers) {
+				if(customer.c == c) {
+					customer.s = CustomerState.Leaving;
+					stateChanged();
+				}
+			}
+		}
+	}
+	
+	public void msgAtWaitingArea(Customer c) {
+		for(MyCustomer customer : customers) {
+			if(customer.c == c) {
+				customer.s = CustomerState.Waiting;
+				stateChanged();
+			}
+		}
+	}
+	public void msgCustomerSeated(Customer c) {
+		for(MyCustomer customer : customers) {
+			if(customer.c == c) {
+				customer.s = CustomerState.Seated;
+				stateChanged();
+			}
+		}
+	}
+	
+	public void msgTableIsFree(int t) {
+		//set customer state to finished
+		for(Table table : tables) {
+			if(table.getNum() == t) {
+				MyCustomer c= table.getOccupant();
+				c.s = CustomerState.Finished;
+			}
+		}
+		stateChanged();
+	}
+	
+	public void msgWantToGoOnBreak(Waiter w) {
+		synchronized(waiters) {
+			for(MyWaiter waiter : waiters) {
+				if(waiter.w == w) {
+					waiter.s = WaiterState.WantToGoOnBreak;
+					stateChanged();
+				}
+			}
+		}
+	}
+
+	public void msgBackFromBreak(Waiter w) {
+		synchronized(waiters) {
+			for(MyWaiter waiter : waiters) {
+				if(waiter.w == w) {
+					waiter.s = WaiterState.Active;
+					numWaiters++;
+					stateChanged();
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Scheduler.  Determine what action is called for, and do it.
+	 */
+	public boolean pickAndExecuteAnAction() {
+		synchronized(customers) {
+			for(MyCustomer customer : customers) {
+				if(customer.s == CustomerState.Leaving) {
+					removeLeavingCustomer(customer);
+					return true;
+				}
+			}
+		}
+		synchronized(customers) {
+			for(MyCustomer customer : customers) {
+				if(customer.s == CustomerState.Finished) {
+					removeCustomer(customer);
+					return true;
+				}
+			}
+		}
+		synchronized(customers) {
+			for(MyCustomer customer : customers) {
+				if(customer.s == CustomerState.Arrived) {
+					if(restaurantFull())
+						tellCustomerRestaurantIsFull(customer);
+					else
+						showCustomerWaitingArea(customer);
+					return true;
+				}
+			}
+		}
+		synchronized(customers) {
+			for(MyCustomer customer : customers) {
+				if(customer.s == CustomerState.Seated) {
+					removeWaitingCustomer(customer);
+					return true;
+				}
+			}
+		}
+		synchronized(customers) {
+			for(MyCustomer customer : customers) {
+				if(customer.s == CustomerState.WillWait) {
+					showCustomerWaitingArea(customer);
+					return true;
+				}
+			}
+		}
+		synchronized(customers) {
+			for(MyCustomer customer : customers) {
+				if(customer.s == CustomerState.Waiting)  {
+					for(Table table : tables) {
+						if(!table.isOccupied()) {
+							seatCustomer(customer,table);
+							return true;
+						}
+					}
+				}
+			}
+		}
+		synchronized(waiters) {
+			for(MyWaiter waiter : waiters) {
+				if(waiter.s == WaiterState.WantToGoOnBreak) {
+					checkIfBreakPossible(waiter);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/** Actions */
+	void showCustomerWaitingArea(MyCustomer c) {
+		Do("Please wait here.");
+		c.s = CustomerState.Walking;
+		for(Integer i : waitingAreas.keySet()) {
+			if(waitingAreas.get(i) == null) {
+				waitingAreas.put(i, c);
+				c.n = i;
+				c.c.msgPleaseWaitHere(c.n);
+				break;
+			}
+		}
+		/*
+		synchronized(waitingPositions) {
+		for(MyCustomer customer : waitingPositions) {
+			if(customer == null) {
+				customer = c;
+				c.n = waitingPositions.indexOf(customer);
+				c.c.msgPleaseWaitHere(c.n);
+				return;
+			}
+		}
+		}
+		waitingPositions.add(c);
+		c.n = waitingPositions.indexOf(c);*/
+
+	}
+	
+	void seatCustomer(MyCustomer c, Table t) {
+		if(waiters.isEmpty())
+			return;
+		//checks if there is an available waiter
+		Do("Seating " + c.getName());
+		c.s = CustomerState.Seating;
+		t.setOccupant(c);
+		//finds waiter with the lowest customer count
+		MyWaiter waiter = waiters.get(0);
+		//switches first waiter to second if 1 on break
+		if(waiter.s == WaiterState.OnBreak)
+			waiter = waiters.get(1);
+		synchronized(waiters) {
+			for(MyWaiter w : waiters) {
+				if(w.s == WaiterState.Active) {
+					if((w.getCount() < waiter.getCount()))
+						waiter = w;
+				}
+			}
+		}
+		c.w = waiter;
+		waiter.w.msgPleaseSeatCustomer(c.c,c.n,t.getNum());
+		waiter.increaseCount();
+	}
+	
+	void removeWaitingCustomer(MyCustomer c) {
+		Do("Removing customer off waitlist.");
+		c.s = CustomerState.Eating;
+		waitingAreas.put(c.n,null);
+	}
+	
+	void tellCustomerRestaurantIsFull(MyCustomer c) {
+		Do("Telling customer restaurant is full.");
+		c.s = CustomerState.Debating;
+		c.c.msgRestaurantFull();
+	}
+	
+	void removeLeavingCustomer(MyCustomer c) {
+		Do("Removing customer who left.");
+		customers.remove(c);
+	}
+	
+	void removeCustomer(MyCustomer c) {
+		//changes customer status to done, sets table unoccupied
+		Do("Removing " + c.getName() + ".");
+		c.s = CustomerState.Done;
+		c.w.decreaseCount();
+		for(Table t : tables) {
+			if(t.getOccupant() == c) {
+				t.setUnoccupied();
+			}
+		}
+	}
+
+	void checkIfBreakPossible(MyWaiter w) {
+		Do("Checking if waiter can go on break.");
+		if(numWaiters == 1) {
+			//send message saying no
+			w.s = WaiterState.Active;
+			w.w.msgNoBreak();
+		}
+		else {
+			//send message saying yes
+			w.s = WaiterState.OnBreak;
+			numWaiters--;
+			w.w.msgGoOnBreak();
+		}
+	}
+	
+	/** Utilities */
+	public String getName() {
+		return mPerson.getName();
+	}
+
+	public List<MyCustomer> getCustomers() {
+		return customers;
+	}
+
+	public Collection<Table> getTables() {
+		return tables;
+	}
+
+	public void addWaiter(Waiter w) {		//add hack for waiters currently
+		synchronized(waiters) {
+			waiters.add(new MyWaiter(w));
+		}
+		numWaiters++;
+	}
+	
+	private boolean restaurantFull() {
+		for(Table table : tables) {
+			if(!table.isOccupied())
+				return false;
+		}
+		return true;
+	}
+	
+	public HostGui_at getGui() {
+		return hostGui;
+	}
+	
+	public void setGui(HostGui_at g) {
+		hostGui = g;
+	}
+	
+	/** Classes */
+	
+	private class MyCustomer {
+		Customer c;
+		CustomerState s;
+		MyWaiter w;
+		boolean willWait = false;
+		int n;
+		
+		MyCustomer(Customer c, CustomerState s) {
+			this.c = c;
+			this.w = null;
+			this.s = s;
+			n = 0;
+		}
+		
+		String getName() {
+			return c.getName();
+		}
+	}
+	
+	private class MyWaiter {
+		Waiter w;
+		int customerCount;
+		WaiterState s;
+		
+		MyWaiter(Waiter w) {
+			this.w = w;
+			customerCount = 0;
+			s = WaiterState.Active;
+		}
+
+		void increaseCount() {
+			customerCount++;
+		}
+		
+		void decreaseCount() {
+			customerCount--;
+		}
+		
+		int getCount() {
+			return customerCount;
+		}
+	}
+	
+	private class Table {
+		MyCustomer occupiedBy;
+		int tableNumber;
+		
+		Table(int tableNumber) {
+			this.tableNumber = tableNumber;
+		}
+
+		void setOccupant(MyCustomer c) {
+			occupiedBy = c;
+		}
+
+		void setUnoccupied() {
+			occupiedBy = null;
+		}
+
+		MyCustomer getOccupant() {
+			return occupiedBy;
+		}
+
+		boolean isOccupied() {
+			return occupiedBy != null;
+		}
+
+		int getNum() {
+			return tableNumber;
+		}
+		
+		public String toString() {
+			return "table " + tableNumber;
+		}
+	}
+}
+
