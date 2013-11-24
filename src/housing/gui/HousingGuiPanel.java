3bf5436d363d3eb11bd6ab960f9cd4df95ae9114
Maggi Yang <maggiyan@usc.edu>
2013-11-24 03:09:54 -0800

    update HousingGui

--- src/housing/gui/HousingGuiPanel.java
+++ src/housing/gui/HousingGuiPanel.java
@@ -58,10 +58,10 @@ public class HousingGuiPanel extends CityCard implements ActionListener {
 		PersonAgent mPerson = new PersonAgent();
 		HousingRenterRole renter1 = new HousingRenterRole(mPerson);
 		HousingPersonGui gui1 = new HousingPersonGui();
-		renter1.setGui(gui1);
+		gui1.setPresent(true); 
 		this.addGui(gui1);
 		
-		renter1.mHungry = true; 
+		//renter1.mHungry = true; 
 	}
 
 	public void actionPerformed(ActionEvent e) {
