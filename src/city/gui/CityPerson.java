cbf42f7375d645c96f616698a4e53715e75d2335
jerryweb <jerryweb@usc.edu>
2013-11-23 17:19:47 -0800

    A person square now appears within the gui, however, it is not linked to the Person Agent yet

--- src/city/gui/CityPerson.java
+++ src/city/gui/CityPerson.java
@@ -1,13 +1,16 @@
 package city.gui;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
 import base.Location;
 import base.PersonAgent;
 
-public class CityPerson implements Gui{
+public class CityPerson extends CityComponent{
 	
+	private String name = "";
 	private PersonAgent agent = null;
 	private boolean atDestination = true;
 	SimCityGui gui;
@@ -22,10 +25,21 @@ public class CityPerson implements Gui{
 	static final int xIndex = 10;
 	static final int yIndex = 10;
 	
+	public CityPerson(int x, int y){
+		super(x,y, Color.ORANGE, "Bob");
+		rectangle = new Rectangle(x, y, 5, 5);
+	}
+	
+	public CityPerson(int x, int y, String ID){
+			super(x,y, Color.ORANGE, ID);
+			rectangle = new Rectangle(x, y, 5, 5);
+			name = ID;
+	}
+	/*
 	public CityPerson(PersonAgent P, SimCityGui gui) {
 		agent = P;
 		this.gui = gui;
-	}
+	}*/
 
 	@Override
 	public void updatePosition() {
@@ -41,7 +55,15 @@ public class CityPerson implements Gui{
         else if (yPos > yDestination)
             yPos--;
 	}
-
+	
+	public void paint(Graphics g) {
+		g.setColor(color);
+		g.fillRect(x, y, 5, 5);
+		//g.fill3DRect(x, y, 20, 20, false);
+		g.setColor(Color.WHITE);
+		g.drawString(name, x - 10, y);
+	}
+	/*
 	@Override
 	public void draw(Graphics2D g) {
         g.setColor(Color.BLACK);
@@ -52,7 +74,7 @@ public class CityPerson implements Gui{
 	public boolean isPresent() {
 		return true;
 	}
-	
+	*/
 	public void DoGoToDestination(int x, int y){
 		atDestination = false;
 		xDestination = x;
@@ -64,6 +86,6 @@ public class CityPerson implements Gui{
 		xDestination = location.mX;
 		yDestination = location.mY;
 	}
-	
+
 
 }
