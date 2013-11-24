5b9a9022c9fc6514ad88be3b1f3705944f90668e
Maggi Yang <maggiyan@usc.edu>
2013-11-23 16:08:25 -0800

    update RenterTest

--- src/housing/test/RenterTest.java
+++ src/housing/test/RenterTest.java
@@ -111,9 +111,10 @@ public class RenterTest extends TestCase {
 		//Check
 		assertEquals("HousingRenter should have one bill", mHousingRenter.mBills.size(), 1); 
 		assertTrue("PAEA: return true and does action", mHousingRenter.pickAndExecuteAnAction()); 
-
+		assertEquals("HousingRenter should pay and then remove bill", mHousingRenter.mBills.size(), 0); 
 		
 	}
+	
 	
 
 }
