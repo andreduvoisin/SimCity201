c58013f9beb817351dfa650aa39743e78dc48661
Angelica <tranac@usc.edu>
2013-11-24 23:56:08 -0800

    Integrating restaurant_tranac.

--- src/restaurant/restaurant_tranac/gui/RestaurantPanel_at.java
+++ src/restaurant/restaurant_tranac/gui/RestaurantPanel_at.java
@@ -3,6 +3,9 @@ package restaurant.restaurant_tranac.gui;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
+import city.gui.CityCard;
+import city.gui.SimCityGui;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,8 +13,11 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Vector;
 
-public class RestaurantPanel_at extends JPanel implements ActionListener {
+import restaurant.restaurant_tranac.roles.*;
+
+public class RestaurantPanel_at extends CityCard implements ActionListener {
 
 	private final int WINDOWX = 626;
 	private final int WINDOWY = 507;
@@ -19,18 +25,36 @@ public class RestaurantPanel_at extends JPanel implements ActionListener {
     private BufferedImage background;
 
     private List<Gui> guis = new ArrayList<Gui>();
+    
+    public RestaurantCashierRole_at mCashier = new RestaurantCashierRole_at();
+    public RestaurantCookRole_at mCook = new RestaurantCookRole_at();
+    public RestaurantHostRole_at mHost = new RestaurantHostRole_at();
+    
+    private HostGui_at hostGui = new HostGui_at(mHost);
+    private CookGui_at cookGui = new CookGui_at(mCook);
+    private CashierGui_at cashierGui = new CashierGui_at(mCashier);
+      
+    private Vector<RestaurantWaiterRole_at> mWaiters = new Vector<RestaurantWaiterRole_at>();
+    private Vector<RestaurantCustomerRole_at> mCustomers = new Vector<RestaurantCustomerRole_at>();
 
-    public RestaurantPanel_at() {
-    	setSize(WINDOWX, WINDOWY);
-        setVisible(true);
-        setBackground(Color.white);
+
+    public RestaurantPanel_at(SimCityGui city) {
+    	super(city);
+    	setBounds(0,0,WINDOWX, WINDOWY);
+   // 	setSize(WINDOWX, WINDOWY);
+   //   setVisible(true);
+    	setBackground(Color.white);
+    	
+    	guis.add(hostGui);
+    	guis.add(cookGui);
+    	guis.add(cashierGui);
  
     	Timer timer = new Timer(TIMERDELAY, this );
     	timer.start();
     	
     	background = null;
     	try {
-    	java.net.URL imageURL = this.getClass().getClassLoader().getResource("restaurant_tranac/gui/images/restaurant.png");
+    	java.net.URL imageURL = this.getClass().getClassLoader().getResource("restaurant/restaurant_tranac/gui/images/restaurant.png");
     	background = ImageIO.read(imageURL);
     	}
     	catch (IOException e) {
@@ -42,11 +66,11 @@ public class RestaurantPanel_at extends JPanel implements ActionListener {
 		repaint();
 	}
 
-    public void paintComponent(Graphics g) {
+    public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D)g;
         
-        g2.drawImage(background,0,0,null);
-
+        if(background != null)
+        	g2.drawImage(background,0,0,null);
         for(Gui gui : guis) {
             if (gui.isPresent()) {
                 gui.updatePosition();
@@ -60,23 +84,15 @@ public class RestaurantPanel_at extends JPanel implements ActionListener {
         }
     }
 
-    public void addGui(CustomerGui_at gui) {
-        guis.add(gui);
-    }
-
-    public void addGui(WaiterGui_at gui) {
+    public void addGui(Gui gui) {
         guis.add(gui);
     }
     
-    public void addGui(CookGui_at gui) {
-    	guis.add(gui);
+    public void addCustomer(RestaurantCustomerRole_at c) {
+    	mCustomers.add(c);
     }
     
-    public void addGui(HostGui_at gui) {
-    	guis.add(gui);
+    public void addWaiter(RestaurantWaiterRole_at w) {
+    	mWaiters.add(w);
     }
-
-	public void addGui(CashierGui_at gui) {
-		guis.add(gui);
-	}
 }
