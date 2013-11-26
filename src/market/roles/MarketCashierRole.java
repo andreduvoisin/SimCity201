98cb3b72f4512059402f3961a8ab53de635d6703
Rex Xu <ttrojan@usc.edu>
2013-11-26 05:45:36 -0800

    added SharedWaiter test maggiyan

--- src/market/roles/MarketCashierRole.java
+++ src/market/roles/MarketCashierRole.java
@@ -58,7 +58,7 @@ public class MarketCashierRole extends BaseRole implements MarketCashier{
 		super(person);
 		mPerson = person;
 		mMarketType = type;
-		mBankAccount = person.getSSN();
+		//mBankAccount = person.getSSN();
 		
 		//populate inventory
 		if(mMarketType == EnumMarketType.FOOD) {
