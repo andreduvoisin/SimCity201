fc088d08f8cf0ae449b0bfcc10fb87cf3020af84
Chase Wagoner <cwagoner@usc.edu>
2013-11-26 10:12:00 -0800

    Removed double import

--- src/city/gui/CityView.java
+++ src/city/gui/CityView.java
@@ -19,7 +19,6 @@ import restaurant.restaurant_duvoisin.gui.AndreRestaurantGui;
 import restaurant.restaurant_jerryweb.gui.JerrywebRestaurantGui;
 import restaurant.restaurant_maggiyan.gui.MaggiyanRestaurantGui;
 import restaurant.restaurant_smileham.gui.SmilehamAnimationPanel;
-import restaurant.restaurant_cwagoner.gui.CwagonerRestaurantGui;
 //import restaurant.restaurant_tranac.gui.TranacRestaurantPanel; // ANGELICA: error here
 import restaurant.restaurant_xurex.gui.RexAnimationPanel;
 import bank.gui.BankPanel;
