fb98942017a96c5203b5018cd55e2ec83d3e8bd1
Angelica <tranac@usc.edu>
2013-11-26 02:53:15 -0800

    Debugged inventory problem.

--- src/restaurant/restaurant_tranac/roles/RestaurantCookRole_at.java
+++ src/restaurant/restaurant_tranac/roles/RestaurantCookRole_at.java
@@ -50,7 +50,11 @@ public class RestaurantCookRole_at extends RestaurantCookRole implements Cook {
                 inventory.add(new Food("Pizza",(int)(baseTime*1.5),DEFAULT_FOOD_QTY));
                 */
                 //inventory created in restaurantCookRole
-                
+                mItemInventory.put(EnumItemType.STEAK,DEFAULT_FOOD_QTY);
+                mItemInventory.put(EnumItemType.CHICKEN,DEFAULT_FOOD_QTY);
+                mItemInventory.put(EnumItemType.SALAD,DEFAULT_FOOD_QTY);
+                mItemInventory.put(EnumItemType.PIZZA,DEFAULT_FOOD_QTY);
+
                 //create cook times
                 mCookTimes.put(EnumItemType.STEAK,(int)(baseTime*2));
                 mCookTimes.put(EnumItemType.CHICKEN,(int)(baseTime*1.75));
