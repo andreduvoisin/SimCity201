5dbab9d53ea829b09af643b9e856f07ff1834a13
sciencepro <dcarr@scienceprousa.com>
2013-11-26 16:26:07 -0800

    housing scenario working

--- src/base/PersonAgent.java
+++ src/base/PersonAgent.java
@@ -44,6 +44,7 @@ public class PersonAgent extends Agent implements Person {
 	//Static data
 	public static int sSSN = 0;
 	public static int sRestaurantCounter = 0;
+	public static int sHouseCounter = 0;
 	
 	//Roles and Job
 	public static enum EnumJobType {BANK, BANKCUSTOMER, HOUSING, MARKET, RESTAURANT, RESTAURANTCUSTOMER, TRANSPORTATION, NONE};
@@ -140,6 +141,9 @@ public class PersonAgent extends Agent implements Person {
 				case HOUSING: 
 					mJobRole = (HousingBaseRole) SortingHat.getHousingRole(this); //get housing status
 					mJobRole.setPerson(this);
+					((HousingBaseRole) mJobRole).setHouse(SimCityGui.getInstance().citypanel.masterHouseList.get(sHouseCounter));
+					mEvents.add(new Event(EnumEventType.MAINTAIN_HOUSE, 0));
+					sHouseCounter++;
 					break;
 				case NONE:
 					break;
@@ -406,7 +410,7 @@ public class PersonAgent extends Agent implements Person {
 	}
 	
 	public void goToJob() {
-		System.out.println("Going to Job");
+//		System.out.println("Going to Job");
 		if (mJobLocation != null){
 			mPersonGui.DoGoToDestination(mJobLocation);
 		}else{
@@ -540,9 +544,10 @@ public class PersonAgent extends Agent implements Person {
 	}
 	
 	public void invokeMaintenance() {
-		if (mHouseRole.mHouse != null) {
-			mHouseRole.msgTimeToMaintain(); //this role is always active
-		}
+//		if (mHouseRole.mHouse != null) {
+			((HousingBaseRole) mJobRole).msgTimeToMaintain();
+//			mHouseRole.msgTimeToMaintain(); //this role is always active
+//		}
 	}
 	
 	private List<Person> getBestFriends(){
