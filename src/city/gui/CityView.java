d381a98cc77c3cf7eebf6606a83eda5d567cab82
Maggi Yang <maggiyan@usc.edu>
2013-12-04 19:31:34 -0800

    add restaurant to cityview

--- src/city/gui/CityView.java
+++ src/city/gui/CityView.java
@@ -14,6 +14,7 @@ import javax.swing.JPanel;
 import market.gui.MarketPanel;
 import market.gui.MarketPanel.EnumMarketType;
 import restaurant.restaurant_davidmca.gui.DavidAnimationPanel;
+import restaurant.restaurant_maggiyan.gui.MaggiyanAnimationPanel;
 import restaurant.restaurant_smileham.gui.SmilehamAnimationPanel;
 import restaurant.restaurant_tranac.gui.TranacAnimationPanel;
 import bank.gui.BankPanel;
@@ -57,9 +58,8 @@ public class CityView extends JPanel implements MouseListener, ActionListener {
 //		cards.put("R_jerryweb", jerryweb);
 //		
 //		//Rest 3		
-//		MaggiyanRestaurantGui maggiyan = new MaggiyanRestaurantGui(city); 
-//		city.citypanel.masterRestaurantList.add(maggiyan.restPanel);
-//		cards.put("R_Maggiyan", maggiyan);
+		MaggiyanAnimationPanel maggiyan = new MaggiyanAnimationPanel(city);
+		cards.put("R_Maggiyan", maggiyan);
 		
 		//Rest 4
 		DavidAnimationPanel davidmca = new DavidAnimationPanel(city);
