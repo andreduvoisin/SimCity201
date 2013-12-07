351aa4839edfc91d956dc9fc08d3b63ee88b0cb8
sciencepro <dcarr@scienceprousa.com>
2013-12-06 19:20:40 -0800

    fix housing, run contactlist setup

--- src/base/reference/House.java
+++ src/base/reference/House.java
@@ -1,5 +1,27 @@
 package base.reference;
 
-public class House {
+import housing.interfaces.HousingRenter;
+import city.gui.CityHousing;
 
+public class House {
+	
+	public double mRent;
+	public int mHouseNum;
+	public HousingRenter mOccupant;
+	public enum HousingType{Apartment, House}; 
+	public HousingType type; 
+	public final double maxAptRent = 450.00;  
+	public CityHousing cityhouse;
+	
+	public House(int ID, double rent) {
+		mHouseNum = ID;
+		mOccupant = null;
+		mRent = rent;
+		if(rent <= maxAptRent){
+			type = HousingType.Apartment; 
+		}
+		else{
+			type = HousingType.House; 
+		}	
+	}
 }
