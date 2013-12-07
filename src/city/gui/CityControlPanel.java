a1b5edb9c61a9638c06e4187921507ecdf574a58
Andre Duvoisin <duvoisin@usc.edu>
2013-12-06 19:08:53 -0800

    removed errors

--- src/city/gui/CityControlPanel.java
+++ src/city/gui/CityControlPanel.java
@@ -3,6 +3,7 @@ package city.gui;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
