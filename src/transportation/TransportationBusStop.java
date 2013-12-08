661f7a401b10c1edcc3dd79e59afc7f7ed4e1e03
Chase Wagoner <cwagoner@usc.edu>
2013-12-07 16:31:04 -0800

    Removed unnecessary shTHINGS

--- src/transportation/TransportationBusStop.java
+++ src/transportation/TransportationBusStop.java
@@ -6,17 +6,11 @@ import transportation.interfaces.TransportationRider;
 import base.Location;
 
 public class TransportationBusStop {
-	static int sStopNumber = 0;
-	final int mStopNumber;
+	// UNNECESSARY?
 	public Location location = new Location(0, 0);
 	ArrayList<TransportationRider> mWaitingPeople = new ArrayList<TransportationRider>();
 	
 	public TransportationBusStop(Location loc) {
 		location.setTo(loc);
-		mStopNumber = sStopNumber++;
-	}
-
-	public int getStopNumber() {
-		return mStopNumber;
 	}
 }
