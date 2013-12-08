0e9cc5a11d5279e0ebc21ef7dbf25521ba024ad0
Maggi Yang <maggiyan@usc.edu>
2013-12-08 03:56:31 -0800

    put back market

--- src/base/PersonAgent.java
+++ src/base/PersonAgent.java
@@ -519,7 +519,7 @@ public class PersonAgent extends Agent implements Person {
 	}
 	
 	private void goToMarket() {
-		mCommuterRole.mActive = true;
+		//mCommuterRole.mActive = true;
 		
 		//ANGELICA: hack
 		mItemsDesired.put(EnumItemType.SALAD, sBaseWanted);
@@ -531,12 +531,12 @@ public class PersonAgent extends Agent implements Person {
 		} else {
 			location = ContactList.getDoorLocation(ContactList.cMARKET2_LOCATION);
 		}
-//		if(!SimCityGui.TESTING){
-//			mPersonGui.DoGoToDestination(location);
-//			acquireSemaphore(semAnimationDone);
-//		}
-//		mPersonGui.setPresent(false); //set city person invisible
-//		
+		if(!SimCityGui.TESTING){
+			mPersonGui.DoGoToDestination(location);
+			acquireSemaphore(semAnimationDone);
+		}
+		mPersonGui.setPresent(false); //set city person invisible
+		
 		//activate marketcustomer role
 		for (Role iRole : mRoles.keySet()){
 			if (iRole instanceof MarketCustomer){
