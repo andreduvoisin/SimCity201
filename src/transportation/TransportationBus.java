584b9d299c22af72d55cc1839e5a4f012428fc14
Chase Wagoner <cwagoner@usc.edu>
2013-12-09 20:51:50 -0800

    Some tests

--- src/transportation/TransportationBus.java
+++ src/transportation/TransportationBus.java
@@ -7,7 +7,6 @@ import java.util.List;
 import test.mock.EventLog;
 import test.mock.LoggedEvent;
 import transportation.interfaces.TransportationRider;
-import transportation.roles.CommuterRole;
 import base.Agent;
 import city.gui.CityBus;
 import city.gui.trace.AlertTag;
@@ -83,7 +82,7 @@ public class TransportationBus extends Agent {
 	 * @param riderDestination The stop number the Person is going to
 	 */
 	public void msgImOn(TransportationRider r) {
-		log.add(new LoggedEvent("Received msgImOn(" + ((CommuterRole)r).getName() + ")"));
+		log.add(new LoggedEvent("Received msgImOn(" + r.getName() + ")"));
 
 		synchronized(mBusStops.get(mCurrentStop).mWaitingPeople) {
 			mBusStops.get(mCurrentStop).mWaitingPeople.remove(r);
@@ -164,8 +163,8 @@ public class TransportationBus extends Agent {
 			for (TransportationRider r : mBusStops.get(mCurrentStop).mWaitingPeople) {
 				r.msgBoardBus();
 			}
+			mBusStops.get(mCurrentStop).mWaitingPeople.clear();
 		}
-		mBusStops.get(mCurrentStop).mWaitingPeople.clear();
 
 		state = enumState.ReadyToTravel;
 		stateChanged();
@@ -179,7 +178,7 @@ public class TransportationBus extends Agent {
 		log.add(new LoggedEvent(("AdvanceToNextStop()")));
 
 		state = enumState.traveling;
-		mGui.DoAdvanceToNextStop();
+		if (! testing) mGui.DoAdvanceToNextStop();
 		// Moved delay to CityBus.DoAdvanceToNextStop()
 	}
 
