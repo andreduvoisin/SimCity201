ca8e100f08a8ede9dc8a14fbc71120e867e0b27e
Chase Wagoner <cwagoner@usc.edu>
2013-12-10 02:35:23 -0800

    Fully working normative scenario; removed some unnecessary stuff from mocks

--- src/transportation/test/mock/MockBus.java
+++ src/transportation/test/mock/MockBus.java
@@ -3,10 +3,8 @@ package transportation.test.mock;
 import java.util.ArrayList;
 import java.util.List;
 
-import city.gui.CityBus;
 import restaurant.restaurant_jerryweb.test.mock.LoggedEvent;
 import test.mock.Mock;
-import transportation.TransportationBus;
 import transportation.interfaces.Bus;
 import transportation.interfaces.TransportationRider;
 
@@ -37,29 +35,4 @@ public class MockBus extends Mock implements Bus {
 	public void msgImOff(TransportationRider r) {
 		log.add(new LoggedEvent("Rider " + r.getName() + " has sent msgImOff."));
 	}
-
-	@Override
-	public boolean pickAndExecuteAnAction() {
-		log.add(new LoggedEvent("PAEA has been called and will return false"));
-		return false;
-	}
-
-	@Override
-	public String getName() {
-		log.add(new LoggedEvent("getName was called, will return null"));
-		return null;
-	}
-
-	@Override
-	public CityBus getBusGui() {
-		log.add(new LoggedEvent("getBusGui was called, will return null"));
-		return null;
-	}
-
-	@Override
-	public TransportationBus getInstance() {
-		log.add(new LoggedEvent("getInstance was called, will return null"));
-		return null;
-	}
-
 }
