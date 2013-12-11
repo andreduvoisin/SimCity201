8663f9917f32dfe9a8bcc0c42647ecb50163898e
Shane Mileham <smileham@usc.edu>
2013-12-10 23:15:35 -0800

    Fixed imports

--- src/restaurant/restaurant_smileham/test/WaiterTest.java
+++ src/restaurant/restaurant_smileham/test/WaiterTest.java
@@ -1,16 +1,10 @@
 package restaurant.restaurant_smileham.test;
 
 import junit.framework.TestCase;
-import restaurant.intermediate.RestaurantCookRole;
-import restaurant.restaurant_smileham.Order;
-import restaurant.restaurant_smileham.Order.EnumOrderStatus;
-import restaurant.restaurant_smileham.Table;
-import restaurant.restaurant_smileham.roles.SmilehamCookRole;
 import restaurant.restaurant_smileham.roles.SmilehamWaiterBase;
 import restaurant.restaurant_smileham.roles.SmilehamWaiterRole;
 import restaurant.restaurant_smileham.test.mock.SmilehamMockCook;
 import restaurant.restaurant_smileham.test.mock.SmilehamMockCustomer;
-import restaurant.restaurant_smileham.test.mock.SmilehamMockWaiter;
 import base.PersonAgent;
 
 public class WaiterTest extends TestCase {
