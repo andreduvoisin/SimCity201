5680f0847cabe2cd8e51cacc64e1c84679b4a8c4
Shane Mileham <smileham@usc.edu>
2013-12-10 22:37:12 -0800

    Working on cook test

--- src/restaurant/restaurant_smileham/test/CookTest.java
+++ src/restaurant/restaurant_smileham/test/CookTest.java
@@ -1,13 +1,27 @@
 package restaurant.restaurant_smileham.test;
 
+import junit.framework.TestCase;
 import restaurant.intermediate.RestaurantCookRole;
 import restaurant.restaurant_smileham.roles.SmilehamCookRole;
+import restaurant.restaurant_smileham.test.mock.SmilehamMockCustomer;
+import restaurant.restaurant_smileham.test.mock.SmilehamMockWaiter;
 import base.PersonAgent;
 
-public class CookTest {
+public class CookTest extends TestCase {
 
 	PersonAgent person;
 	RestaurantCookRole cookRole;
 	SmilehamCookRole cook;
+	SmilehamMockWaiter waiter;
+	SmilehamMockCustomer customer;
+	
+	public void setUp() throws Exception {
+		super.setUp();
+		
+		person = new PersonAgent();
+		cookRole = new RestaurantCookRole(person, 5);
+		cook = new SmilehamCookRole(person, cookRole);
+		waiter = new SmilehamMockWaiter("Waiter");
+	}
 	
 }
