e0b3c5a66218fbe3024f2727e91fcb3711bb2f4e
Chase Wagoner <cwagoner@usc.edu>
2013-12-10 22:55:34 -0800

    Added some testing stuff

--- src/restaurant/restaurant_cwagoner/interfaces/CwagonerHost.java
+++ src/restaurant/restaurant_cwagoner/interfaces/CwagonerHost.java
@@ -5,8 +5,4 @@ public interface CwagonerHost {
 	abstract public void msgIWantFood(CwagonerCustomer cust);
 
 	abstract public void msgCustomerGoneTableEmpty(CwagonerCustomer c, int tableNum);
-	
-	abstract public boolean pickAndExecuteAnAction();
-
-	public abstract void addWaiter(CwagonerWaiter w);
 }
