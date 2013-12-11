a9a91b67d303b14d49687d2572f20f5a1a073971
Shane Mileham <smileham@usc.edu>
2013-12-10 23:15:18 -0800

    Working on waiter test

--- src/restaurant/restaurant_smileham/test/WaiterTest.java
+++ src/restaurant/restaurant_smileham/test/WaiterTest.java
@@ -6,28 +6,36 @@ import restaurant.restaurant_smileham.Order;
 import restaurant.restaurant_smileham.Order.EnumOrderStatus;
 import restaurant.restaurant_smileham.Table;
 import restaurant.restaurant_smileham.roles.SmilehamCookRole;
+import restaurant.restaurant_smileham.roles.SmilehamWaiterBase;
+import restaurant.restaurant_smileham.roles.SmilehamWaiterRole;
+import restaurant.restaurant_smileham.test.mock.SmilehamMockCook;
 import restaurant.restaurant_smileham.test.mock.SmilehamMockCustomer;
 import restaurant.restaurant_smileham.test.mock.SmilehamMockWaiter;
 import base.PersonAgent;
 
 public class WaiterTest extends TestCase {
 	PersonAgent person;
-	RestaurantCookRole cookRole;
-	SmilehamCookRole cook;
-	SmilehamMockWaiter waiter;
+	SmilehamWaiterBase waiter;
+	SmilehamMockCook cook;
 	SmilehamMockCustomer customer;
-	Order order;
-	Table table;
+	
+	
 	
 	public void setUp() throws Exception {
 		super.setUp();
 		person = new PersonAgent();
-		cookRole = new RestaurantCookRole(person, 5);
-		cook = new SmilehamCookRole(person, cookRole);
-		waiter = new SmilehamMockWaiter("Waiter");
-		table = new Table(0);
-		customer = new SmilehamMockCustomer("mock customer");
-		order = new Order(waiter, table, customer, EnumOrderStatus.PENDING);
+		
 		
 	}
+	
+	public void testRegularWaiter() {
+		waiter = new SmilehamWaiterRole(person);
+		
+	}
+	
+	public void testSharedWaiter(){
+		
+	}
+	
+	
 }
